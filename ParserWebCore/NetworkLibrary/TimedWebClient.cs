8d81af94273ec405571f1655a7fcc795cf21a688
rummolprod999 <rummolprod999@gmail.com>
2018-07-13 16:22:52 +0300

    update project

---
+++ ParserWebCore/NetworkLibrary/TimedWebClient.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net;
+
+namespace ParserWebCore.NetworkLibrary
+{
+    public class TimedWebClient : WebClient
+    {
+        protected override WebRequest GetWebRequest(Uri address)
+        {
+            var wr = base.GetWebRequest(address);
+            wr.Timeout = 600000;
+            return wr;
+        }
+    }
+
+    public class TimedWebClientUa : WebClient
+    {
+        protected override WebRequest GetWebRequest(Uri address)
+        {
+            var wr = (HttpWebRequest) base.GetWebRequest(address);
+            wr.Timeout = 600000;
+            wr.UserAgent = "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:55.0) Gecko/20100101 Firefox/55.0";
+            return wr;
+        }
+    }
+}

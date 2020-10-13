b3ebd56a7cf8892a9945f1abf351593cbf0d58d2
rummolprod999 <rummolprod999@gmail.com>
2020-10-13 19:01:44 +0300

    new parser

--- ParserWebCore/Parser/ParserIsMt.cs
+++ ParserWebCore/Parser/ParserIsMt.cs
@@ -1,13 +1,33 @@
+using System;
+using ParserWebCore.Logger;
+
 namespace ParserWebCore.Parser
 {
     public class ParserIsMt: ParserAbstract, IParser
     {
+        private const int Count = 5;
         public void Parsing()
         {
             Parse(ParsingIsmt);
         }
 
         private void ParsingIsmt()
+        {
+            for (var i = 1; i <= Count; i++)
+            {
+                var urlpage = $"http://is-mt.pro/Purchase/ListPurchase?Page={i}&SearchType=Purchase";
+                try
+                {
+                    ParsingPage(urlpage);
+                }
+                catch (Exception e)
+                {
+                    Log.Logger(e);
+                }
+            }
+        }
+
+        private void ParsingPage(string url)
         {
         }
     }

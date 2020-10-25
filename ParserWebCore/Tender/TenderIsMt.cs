5b60ba2ef725d1a3742bc8263b1ff24828a85d35
rummolprod999 <rummolprod999@gmail.com>
2020-10-25 10:11:34 +0300

    add new place

--- ParserWebCore/Tender/TenderIsMt.cs
+++ ParserWebCore/Tender/TenderIsMt.cs
@@ -1,8 +1,11 @@
 using System;
 using System.Data;
+using HtmlAgilityPack;
 using MySql.Data.MySqlClient;
 using ParserWebCore.BuilderApp;
 using ParserWebCore.Connections;
+using ParserWebCore.Logger;
+using ParserWebCore.NetworkLibrary;
 using ParserWebCore.TenderType;
 
 namespace ParserWebCore.Tender
@@ -37,6 +40,112 @@ namespace ParserWebCore.Tender
                 {
                     return;
                 }
+                var s = DownloadString.DownLUserAgent(_tn.Href);
+                if (string.IsNullOrEmpty(s))
+                {
+                    Log.Logger("Empty string in ParsingTender()", _tn.Href);
+                    return;
+                }
+
+                var htmlDoc = new HtmlDocument();
+                htmlDoc.LoadHtml(s);
+                var navigator = (HtmlNodeNavigator) htmlDoc.CreateNavigator();
+                var dateUpd = DateTime.Now;
+                var cancelStatus = 0;
+                var updated = false;
+                var selectDateT =
+                    $"SELECT id_tender, date_version, cancel FROM {Builder.Prefix}tender WHERE purchase_number = @purchase_number AND type_fz = @type_fz";
+                var cmd2 = new MySqlCommand(selectDateT, connect);
+                cmd2.Prepare();
+                cmd2.Parameters.AddWithValue("@purchase_number", _tn.PurNum);
+                cmd2.Parameters.AddWithValue("@type_fz", TypeFz);
+                var adapter2 = new MySqlDataAdapter {SelectCommand = cmd2};
+                var dt2 = new DataTable();
+                adapter2.Fill(dt2);
+                foreach (DataRow row in dt2.Rows)
+                {
+                    updated = true;
+                    if (dateUpd >= (DateTime) row["date_version"])
+                    {
+                        row["cancel"] = 1;
+                    }
+                    else
+                    {
+                        cancelStatus = 1;
+                    }
+                }
+
+                var commandBuilder =
+                    new MySqlCommandBuilder(adapter2) {ConflictOption = ConflictOption.OverwriteChanges};
+                adapter2.Update(dt2);
+                var printForm = _tn.Href;
+                var customerId = 0;
+                var organiserId = 0;
+                var orgName = EtpName;
+                if (!string.IsNullOrEmpty(orgName))
+                {
+                    var selectOrg =
+                        $"SELECT id_organizer FROM {Builder.Prefix}organizer WHERE full_name = @full_name";
+                    var cmd3 = new MySqlCommand(selectOrg, connect);
+                    cmd3.Prepare();
+                    cmd3.Parameters.AddWithValue("@full_name", orgName);
+                    var dt3 = new DataTable();
+                    var adapter3 = new MySqlDataAdapter {SelectCommand = cmd3};
+                    adapter3.Fill(dt3);
+                    if (dt3.Rows.Count > 0)
+                    {
+                        organiserId = (int) dt3.Rows[0].ItemArray[0];
+                    }
+                    else
+                    {
+                        var phone = "";
+                        var email = "";
+                        var inn = "";
+                        var kpp = "";
+                        var contactPerson = "";
+                        var addOrganizer =
+                            $"INSERT INTO {Builder.Prefix}organizer SET full_name = @full_name, contact_phone = @contact_phone, contact_person = @contact_person, contact_email = @contact_email, inn = @inn, kpp = @kpp";
+                        var cmd4 = new MySqlCommand(addOrganizer, connect);
+                        cmd4.Prepare();
+                        cmd4.Parameters.AddWithValue("@full_name", orgName);
+                        cmd4.Parameters.AddWithValue("@contact_phone", phone);
+                        cmd4.Parameters.AddWithValue("@contact_person", contactPerson);
+                        cmd4.Parameters.AddWithValue("@contact_email", email);
+                        cmd4.Parameters.AddWithValue("@inn", inn);
+                        cmd4.Parameters.AddWithValue("@kpp", kpp);
+                        cmd4.ExecuteNonQuery();
+                        organiserId = (int) cmd4.LastInsertedId;
+                    }
+                }
+                
+                GetEtp(connect, out var idEtp);
+                var insertTender =
+                    $"INSERT INTO {Builder.Prefix}tender SET id_region = @id_region, id_xml = @id_xml, purchase_number = @purchase_number, doc_publish_date = @doc_publish_date, href = @href, purchase_object_info = @purchase_object_info, type_fz = @type_fz, id_organizer = @id_organizer, id_placing_way = @id_placing_way, id_etp = @id_etp, end_date = @end_date, scoring_date = @scoring_date, bidding_date = @bidding_date, cancel = @cancel, date_version = @date_version, num_version = @num_version, notice_version = @notice_version, xml = @xml, print_form = @print_form";
+                var cmd9 = new MySqlCommand(insertTender, connect);
+                cmd9.Prepare();
+                cmd9.Parameters.AddWithValue("@id_region", 0);
+                cmd9.Parameters.AddWithValue("@id_xml", _tn.PurNum);
+                cmd9.Parameters.AddWithValue("@purchase_number", _tn.PurNum);
+                cmd9.Parameters.AddWithValue("@doc_publish_date", _tn.DatePub);
+                cmd9.Parameters.AddWithValue("@href", _tn.Href);
+                cmd9.Parameters.AddWithValue("@purchase_object_info", _tn.PurName);
+                cmd9.Parameters.AddWithValue("@type_fz", TypeFz);
+                cmd9.Parameters.AddWithValue("@id_organizer", organiserId);
+                cmd9.Parameters.AddWithValue("@id_placing_way", 0);
+                cmd9.Parameters.AddWithValue("@id_etp", idEtp);
+                cmd9.Parameters.AddWithValue("@end_date", _tn.DateEnd);
+                cmd9.Parameters.AddWithValue("@scoring_date", DateTime.MinValue);
+                cmd9.Parameters.AddWithValue("@bidding_date", DateTime.MinValue);
+                cmd9.Parameters.AddWithValue("@cancel", cancelStatus);
+                cmd9.Parameters.AddWithValue("@date_version", dateUpd);
+                cmd9.Parameters.AddWithValue("@num_version", 1);
+                cmd9.Parameters.AddWithValue("@notice_version", _tn.Status);
+                cmd9.Parameters.AddWithValue("@xml", _tn.Href);
+                cmd9.Parameters.AddWithValue("@print_form", printForm);
+                var resInsertTender = cmd9.ExecuteNonQuery();
+                var idTender = (int) cmd9.LastInsertedId;
+                Counter(resInsertTender, updated);
+                
             }
         }
     }

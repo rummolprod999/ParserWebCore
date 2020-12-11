38a03854d7f58a9cb34aab69aa4eb8edca0b143a
rummolprod999 <rummolprod999@gmail.com>
2020-12-11 18:35:48 +0300

    add prs

--- ParserWebCore/Parser/ParserFederal.cs
+++ ParserWebCore/Parser/ParserFederal.cs
@@ -1,7 +1,11 @@
 using System;
 using System.Net;
+using HtmlAgilityPack;
+using ParserWebCore.Extensions;
 using ParserWebCore.Logger;
 using ParserWebCore.NetworkLibrary;
+using ParserWebCore.Tender;
+using ParserWebCore.TenderType;
 
 namespace ParserWebCore.Parser
 {
@@ -40,7 +44,71 @@ namespace ParserWebCore.Parser
         private void GetPage(string url)
         {
             var s = DownloadString.DownLHttpPostWithCookies(url, HttpsT2Federal1Ru, cookie);
-            Console.WriteLine(s);
+            if (string.IsNullOrEmpty(s))
+            {
+                Log.Logger($"Empty string in {GetType().Name}.{System.Reflection.MethodBase.GetCurrentMethod().Name}",
+                    url);
+                return;
+            }
+
+            var htmlDoc = new HtmlDocument();
+            htmlDoc.LoadHtml(s);
+            var tens =
+                htmlDoc.DocumentNode.SelectNodes(
+                    "//table[contains(@class, 'mte-grid-table')]/tbody/tr") ??
+                new HtmlNodeCollection(null);
+            foreach (var a in tens)
+            {
+                try
+                {
+                    ParserTender(a, url);
+                }
+                catch (Exception e)
+                {
+                    Log.Logger(e);
+                }
+            }
+        }
+
+        private void ParserTender(HtmlNode n, string _href)
+        {
+            var purName =
+                n.SelectSingleNode("./td[position() = 3]/a")?.InnerText?.Trim() ??
+                throw new Exception(
+                    $"Cannot find purName in {_href}");
+            var purNum =
+                n.SelectSingleNode("./td[position() = 1]")?.InnerText?.Trim() ??
+                throw new Exception(
+                    $"Cannot find purNum in {_href}");
+            var cusName =
+                n.SelectSingleNode("./td[position() = 2]")?.InnerText?.Trim() ??
+                throw new Exception(
+                    $"Cannot find cusName in {_href}");
+            var nmckT =
+                n.SelectSingleNode("./td[position() = 4]")?.InnerText?.Trim() ?? "";
+            var (nmck, currency) = nmckT.GetTwoDataFromRegex("([\\d\\s.]+)(.*)");
+            nmck = nmck.DelAllWhitespace();
+            var href =
+                n.SelectSingleNode("./td[position() = 3]/a")?.Attributes["href"]?.Value ??
+                throw new Exception(
+                    $"Cannot find href in {_href}");
+            var status =
+                n.SelectSingleNode("./td[position() = 7]")?.InnerText?.Trim() ?? "";
+            var pwName =
+                n.SelectSingleNode("./td[position() = 6]")?.InnerText?.Trim() ?? "";
+            var endDateT =
+                n.SelectSingleNode("./td[position() = 5]")?.InnerText?.Trim() ??
+                throw new Exception(
+                    $"Cannot find endDateT in {_href}");
+            var dateEndT = endDateT.GetDataFromRegex(@"(\d{2}\.\d{2}\.\d{4}\s+\d{2}:\d{2})");
+            var dateEnd = dateEndT.ParseDateUn("dd.MM.yyyy HH:mm");
+            var tn = new TenderFederal("ЭТП \"Федерация\"", "https://t2.federal1.ru/", 294,
+                new TypeFederal
+                {
+                    PurName = purName, PurNum = purNum, DatePub = DateTime.Now, Href = href, DateEnd = dateEnd,
+                    PwName = pwName, Currency = currency, Status = status, Nmck = nmck, CusName = cusName
+                });
+            ParserTender(tn);
         }
     }
 }
--- ParserWebCore/TenderType/TypeFederal.cs
+++ ParserWebCore/TenderType/TypeFederal.cs
@@ -2,5 +2,16 @@ namespace ParserWebCore.TenderType
 {
     public class TypeFederal : AbstractTypeT
     {
+        public string Nmck { get; set; }
+        public string Status { get; set; }
+        public string PwName { get; set; }
+        public string CusName { get; set; }
+        public string Currency { get; set; }
+
+        public override string ToString()
+        {
+            return
+                $"{base.ToString()}, {nameof(Nmck)}: {Nmck}, {nameof(Status)}: {Status}, {nameof(PwName)}: {PwName}, {nameof(CusName)}: {CusName}, {nameof(Currency)}: {Currency}";
+        }
     }
 }

80928300180d2b89d82577eb0bfdcfc83282f31c
rummolprod999 <rummolprod999@gmail.com>
2020-12-27 18:01:04 +0300

    add prs

--- ParserWebCore/Tender/TenderB2BWeb.cs
+++ ParserWebCore/Tender/TenderB2BWeb.cs
@@ -1,4 +1,7 @@
 using System;
+using System.Data;
+using MySql.Data.MySqlClient;
+using ParserWebCore.BuilderApp;
 using ParserWebCore.Connections;
 using ParserWebCore.TenderType;
 
@@ -18,7 +21,23 @@ namespace ParserWebCore.Tender
         {
             using (var connect = ConnectToDb.GetDbConnection())
             {
-                Console.WriteLine(_tn);
+                var dateUpd = DateTime.Now;
+                connect.Open();
+                var selectTend =
+                    $"SELECT id_tender FROM {Builder.Prefix}tender WHERE purchase_number = @purchase_number AND end_date = @end_date AND type_fz = @type_fz AND doc_publish_date = @doc_publish_date";
+                var cmd = new MySqlCommand(selectTend, connect);
+                cmd.Prepare();
+                cmd.Parameters.AddWithValue("@purchase_number", _tn.PurNum);
+                cmd.Parameters.AddWithValue("@end_date", _tn.DateEnd);
+                cmd.Parameters.AddWithValue("@type_fz", TypeFz);
+                cmd.Parameters.AddWithValue("@doc_publish_date", _tn.DatePub);
+                var dt = new DataTable();
+                var adapter = new MySqlDataAdapter {SelectCommand = cmd};
+                adapter.Fill(dt);
+                if (dt.Rows.Count > 0)
+                {
+                    return;
+                }
             }
         }
     }

d7d9ea10839d28584fde87b469ea9df468f920c7
rummolprod999 <rummolprod999@gmail.com>
2021-06-02 06:53:28 +0300

    add new place

--- ParserWebCore/Parser/ParserSochiPark2.cs
+++ ParserWebCore/Parser/ParserSochiPark2.cs
@@ -44,7 +44,7 @@ namespace ParserWebCore.Parser
         private void ParsingPage(string page)
         {
             var arguments =
-                $"\"{page}\" \\\n  -H \"authority: zakup.sochipark.ru\" \\\n  -H \"sec-ch-ua: \" Not;A Brand\";v=\"99\", \"Google Chrome\";v=\"91\", \"Chromium\";v=\"91\"\" \\\n  -H \"sec-ch-ua-mobile: ?0\" \\\n  -H \"upgrade-insecure-requests: 1\" \\\n  -H \"user-agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36\" \\\n  -H \"accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9\" \\\n  -H \"sec-fetch-site: same-origin\" \\\n  -H \"sec-fetch-mode: navigate\" \\\n  -H \"sec-fetch-user: ?1\" \\\n  -H \"sec-fetch-dest: document\" \\\n  -H \"referer: https://zakup.sochipark.ru/purchases/\" \\\n  -H \"accept-language: ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7\"";
+                $"\"{page}\"";
             var docString =
                 CurlDownloadSportMaster.DownL(arguments);
             if (string.IsNullOrEmpty(docString))
@@ -70,7 +70,7 @@ namespace ParserWebCore.Parser
                 try
                 {
                     ParserTender(a);
-                    Thread.Sleep(5000 * 2);
+                    Thread.Sleep(5000 * 4);
                 }
                 catch (Exception e)
                 {
--- ParserWebCore/Tender/TenderSochipark2.cs
+++ ParserWebCore/Tender/TenderSochipark2.cs
@@ -47,7 +47,7 @@ namespace ParserWebCore.Tender
                 var cancelStatus = 0;
                 var updated = false;
                 var arguments =
-                    $"\"{_tn.Href}\" \\\n  -H \"authority: zakup.sochipark.ru\" \\\n  -H \"cache-control: max-age=0\" \\\n  -H \"sec-ch-ua: \" Not;A Brand\";v=\"99\", \"Google Chrome\";v=\"91\", \"Chromium\";v=\"91\"\" \\\n  -H \"sec-ch-ua-mobile: ?0\" \\\n  -H \"upgrade-insecure-requests: 1\" \\\n  -H \"user-agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36\" \\\n  -H \"accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9\" \\\n  -H \"sec-fetch-site: same-origin\" \\\n  -H \"sec-fetch-mode: navigate\" \\\n  -H \"sec-fetch-user: ?1\" \\\n  -H \"sec-fetch-dest: document\" \\\n  -H \"referer: https://zakup.sochipark.ru/purchases/\" \\\n  -H \"accept-language: ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7\"";
+                    $"\"{_tn.Href}\"";
                 var docString =
                     CurlDownloadSportMaster.DownL(arguments);
                 if (string.IsNullOrEmpty(docString))

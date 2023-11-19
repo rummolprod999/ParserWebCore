c143fcccb8b60967cd0ca0522e53d57342310ee2
rummolprod999 <rummolprod999@gmail.com>
2023-11-19 09:53:45 +0300

    fix

--- ParserWebCore/Tender/TenderLotOnline.cs
+++ ParserWebCore/Tender/TenderLotOnline.cs
@@ -55,7 +55,7 @@ namespace ParserWebCore.Tender
                 if (string.IsNullOrEmpty(s))
                 {
                     s = DownloadString.DownLHttpPost(_tn.Href.Replace(
-                        "https://market.lot-online.ru/app/SmallPurchaseCard/page?SmallPurchaseCard.smallPurchaseEntity=",
+                        "https://market.lot-online.ru/etp/app/LotCard/page?LotCard.lotEntity=",
                         "https://market.lot-online.ru/etp/app/section/rosseti/LotCard/page?LotCard.lotEntity="));
                 }
 
